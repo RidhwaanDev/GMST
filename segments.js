ac9197dd05bef2bc4c57d497f040e15215159cec
Ridhwaan Anayetullah <ridhwaan.any@gmail.com>
2019-12-09 14:59:57 -0500

    add some polylines

--- segments.js
+++ segments.js
@@ -1,66 +1,109 @@
 let map;
 
 const locations = {
-  lat : 40.499820,
+  lat: 40.499820,
   lng: -74.448530,
 };
 
 let poly;
 
+const polyine_options = {
+  "A": {
+
+  },
+
+  "LX": {
+
+  },
+}
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 7,
-    center: {lat: locations.lat, lng:locations.lng}  // Center the map on Chicago, USA.
+    zoom: 12,
+    center: { lat: locations.lat, lng: locations.lng }
   });
 
   poly = new google.maps.Polyline({
     strokeColor: '#000000',
     strokeOpacity: 1.0,
-    strokeWeight: 3
+    strokeWeight: 5,
   });
 
-//  poly.setMap(map);
+  getSegmentsForRoute();
+}
 
-  map.addListener('click', addLatLng);
-
-  const triangleCoords = [
-    {lat: 25.774, lng: -80.190},
-    {lat: 18.466, lng: -66.118},
-    {lat: 32.321, lng: -64.757},
-    {lat: 25.774, lng: -80.190}
-  ];
-
-  const bermudaTriangle = new google.maps.Polygon({
-    paths: triangleCoords,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35
+function getSegmentsForRoute() {
+  // query Transloc API
+  $.ajax({
+    url: "https://transloc-api-1-2.p.rapidapi.com/segments.json",
+    type: "GET",
+    contentType: 'application/json; charset=utf-8',
+    data: {
+      "agencies": 1323,
+      "routes": "4012628",
+      "geo_area": "40.506831,-74.456645|15000",
+    },
+    headers: {
+      "X-RapidAPI-Host": "transloc-api-1-2.p.rapidapi.com",
+      "X-RapidAPI-Key": "hHcLr1qWHDmshwibREtIrhryL9bcp1Fw9AQjsnCiZyEzRrJKOS"
+    },
+    
+    success: function (result) {
+      parseSegments(clean(result));
+    }
   });
-  // bermudaTriangle.setMap(map);
-  decode("unfvF|_meMm@fF");
+}
+
+function parseSegments(result) {
+  const segments = result.data;
+  for (let key of Object.keys(segments)) {
+    let array_latlng = google.maps.geometry.encoding.decodePath(segments[key]);
+    array_latlng.forEach(latlng => {addLatLngToPoly(latlng,poly)});
+  }
+  
+  poly.setMap(map);
 
 }
 
-function decode(encodedSegment){
-      let path = google.maps.geometry.encoding.decodePath(encodedSegment);
-      console.log(path);
-}
-
-// Handles click events on a map, and adds a new point to the Polyline.
-function addLatLng(event) {
-  var path = poly.getPath();
-
+function addLatLngToPoly(latLng, poly) {
+  const path = poly.getPath();
   // Because path is an MVCArray, we can simply append a new coordinate
-  // and it will automatically appear.
-  path.push(event.latLng);
-
-  // Add a new marker at the new plotted point on the polyline.
-  var marker = new google.maps.Marker({
-    position: event.latLng,
-    title: '#' + path.getLength(),
-    map: map
-  });
+  // and it will automatically appear
+  path.push(latLng);
 }
 
+function clean(result) {
+  const b = new Buffer(result.data,'binary');
+  const str = b.toString();
+  const regexp = /"(\d{9})"/;
+
+  // replace each 9 digit number (the keys to each segment) with a random key.
+  // This prevents JSON.parse from sorting the keys and messing with the segments
+
+  // count the keys in the sorted/messed up object
+  const result_sorted = JSON.parse(str);
+
+  let i = 0;
+
+  Object.keys(result_sorted['data']).forEach(key => {
+      i++;
+  });
+
+  // do regex replace with "key" + key_cnt on each instance of the key
+  let key_cnt = 0;
+  let prev = str;
+  let running_str = '';
+  while(prev.match(regexp) != null){
+      running_str = prev.replace(regexp,`\"segment${key_cnt}\"`);
+      prev = running_str;
+      key_cnt++;
+  }
+  // compare keys in the unsorted/cleaned object
+  let j = 0;
+  const final_segments = JSON.parse(running_str);
+  Object.keys(final_segments).forEach(key => {
+      j++;
+  });
+
+  return final_segments;
+};
